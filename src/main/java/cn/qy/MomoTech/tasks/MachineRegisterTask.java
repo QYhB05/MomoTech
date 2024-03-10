b87ff2f971cd8a5bd747d8587fdeb44dcdec09c0
QYhB05 <140108356+QYhB05@users.noreply.github.com>
2024-03-10 15:07:09 +0800

    Update MachineRegisterTask.java

--- src/main/java/cn/qy/MomoTech/tasks/MachineRegisterTask.java
+++ src/main/java/cn/qy/MomoTech/tasks/MachineRegisterTask.java
@@ -20,66 +20,66 @@ public class MachineRegisterTask {
     public static void run(@NotNull Logger logger) {
         ItemStack[] empty = {null, null, null, null, null, null, null, null, null};
         ItemStack[] digitalConstructor = {
-                empty_shell, none_, empty_shell,
-                none_, creative_item, none_,
-                empty_shell, none_, empty_shell
+                empty_shell, box_of_quantum, empty_shell,
+                quantum_, creative_item, quantum_,
+                empty_shell, quantum1_, empty_shell
         };
         ItemStack[] ordinaryNumberCombinator = {
-                empty_shell, empty_shell, empty_shell,
+                quantum_, box_of_quantum, quantum1_,
                 SlimefunItems.BIG_CAPACITOR, creative_item_I, SlimefunItems.BIG_CAPACITOR,
                 increment, digital_constructor, increment
         };
         ItemStack[] symbolGenerator = {
-                none_, empty_shell, none_,
-                empty_shell, creative_item, empty_shell,
-                none_, empty_shell, none_,
+                none_, box_of_quantum, none_,
+                quantum1_, creative_item, quantum1_,
+                none_, quantum_, none_,
         };
         ItemStack[] increment = {
-                digital_constructor, symbol_addition, digital_constructor,
+                none_, symbol_addition, none_,
                 SlimefunItems.MAGIC_SUGAR, creative_item, SlimefunItems.MAGIC_SUGAR,
-                none_, empty_shell, none_
+                uncontrollable_empty, uncontrollable_empty, uncontrollable_empty
         };
         ItemStack[] emptyShellGenerator_I = {
-                empty_shell_generator, empty_shell_generator, empty_shell_generator,
+                empty_shell_generator, box_of_quantum, empty_shell_generator,
                 empty_shell_generator, none_, empty_shell_generator,
                 none_, none_, none_
         };
         ItemStack[] noneGenerator = {
                 new ItemStack(Material.DIRT), new ItemStack(Material.DIRT), new ItemStack(Material.DIRT),
-                new ItemStack(Material.GLASS), null, new ItemStack(Material.GLASS),
-                null, empty_shell, null
+                new ItemStack(Material.GLASS), uncontrollable_empty, new ItemStack(Material.GLASS),
+                null, quantum1_, null
 
         };
         ItemStack[] creativeItemGenerator = {
                 new ItemStack(Material.COBBLESTONE), new ItemStack(Material.COBBLESTONE), new ItemStack(Material.COBBLESTONE),
-                new ItemStack(Material.GLASS), null, new ItemStack(Material.GLASS),
+                new ItemStack(Material.GLASS), box_of_quantum, new ItemStack(Material.GLASS),
                 null, empty_shell, null
 
         };
         ItemStack[] quantityConstructor = {
-                creative_item, empty_shell_generator, creative_item_I,
-                qy, null, ch,
+                creative_item, box_of_quantum, creative_item_I,
+                qy, quantum1_, ch,
                 creative_item_generator, uncontrollable_empty, none_generator
         };
-        ItemStack[] emptyShellGenerator = {none_, none_, none_, none_, uncontrollable_empty, none_, none_, none_, none_};
-        ItemStack[] creativeGenerator = {null, null, quantity_item, null, new ItemStack(Material.COMMAND_BLOCK), null, null, null, null};
+        ItemStack[] emptyShellGenerator = {none_, box_of_quantum, none_, none_, uncontrollable_empty, none_, none_, none_, none_};
+        ItemStack[] creativeGenerator = {box_of_quantum, null, quantity_item, null, new ItemStack(Material.COMMAND_BLOCK), null, null, uncontrollable_empty, null};
         ItemStack[] finalCopier = {final_item, final_star, final_item,
                 MomotechItem.increment, random_copier, MomotechItem.increment,
-                bug_crystal, quantity_item, bug_crystal
+                final_item, quantity_item, final_item
         };
-        ItemStack[] uncontrollableEmptyGenerator = {none_, none_, none_,
-                bug_crystal, new ItemStack(Material.COMMAND_BLOCK), bug_crystal,
+        ItemStack[] uncontrollableEmptyGenerator = {box_of_quantum, quantity_item, box_of_quantum,
+                bug_crystal, final_item, bug_crystal,
                 empty_shell_generator, creative_item_II, empty_shell_generator
         };
         ItemStack[] oreCollector = {
-                ch, new ItemStack(Material.COMMAND_BLOCK), ch,
-                ch, qy, ch,
+                ch, final_star, ch,
+                star_, box_of_quantum, star_,
                 ch, quantity_item, ch
         };
         ItemStack[] randomCopier = {
                 bug_crystal, new ItemStack(Material.COMMAND_BLOCK), bug_crystal,
-                final_star, final_item, final_star,
-                final_star, quantity_item, final_star
+                final_star, quantity_item, final_star,
+                final_item, final_item, final_item
         };
         ItemStack[] stoneGenerator = {
                 creative_item_I, none_, creative_item,
@@ -92,7 +92,7 @@ public class MachineRegisterTask {
                 creative_item, none_, creative_item_I,
         };
         ItemStack[] fishGenerator = {
-                creative_item_I, none_, creative_item,
+                creative_item_I, star_, creative_item,
                 empty_shell, new ItemStack(Material.FISHING_ROD), empty_shell,
                 creative_item, uncontrollable_empty, creative_item_I,
         };
@@ -102,17 +102,17 @@ public class MachineRegisterTask {
                 creative_item, uncontrollable_empty, creative_item_I,
         };
         ItemStack[] templateGenerator = {
-                creative_item_I, none_, creative_item,
+                creative_item_I, star_, creative_item,
                 empty_shell, new ItemStack(Material.DIAMOND), empty_shell,
                 creative_item, empty_shell, creative_item_I,
         };
         ItemStack[] OREGenerator = {
-                creative_item_I, none_, creative_item,
+                creative_item_I, star_, creative_item,
                 none_, new ItemStack(Material.DIAMOND_BLOCK), none_,
                 creative_item, uncontrollable_empty, creative_item_I,
         };
         ItemStack[] glassGenerator = {
-                creative_item_I, none_, creative_item,
+                creative_item_I, star_, creative_item,
                 empty_shell, new ItemStack(Material.GLASS), empty_shell,
                 creative_item, uncontrollable_empty, creative_item_I,
         };
@@ -123,67 +123,85 @@ public class MachineRegisterTask {
         };
         ItemStack[] dustGenerator = {
                 creative_item_I, uncontrollable_empty, creative_item,
-                SlimefunItems.BIG_CAPACITOR, new ItemStack(Material.GRAVEL), SlimefunItems.BIG_CAPACITOR,
+                star_, new ItemStack(Material.GRAVEL), star_,
                 creative_item, uncontrollable_empty, creative_item_I,
         };
         ItemStack[] lineGenerator = {
-                creative_item_I, none_, creative_item,
+                creative_item_I, star_, creative_item,
                 uncontrollable_empty, new ItemStack(Material.STRING), uncontrollable_empty,
                 creative_item, none_, creative_item_I,
         };
         ItemStack[] stoneChunkGenerator = {
-                creative_item_I, none_, creative_item,
+                creative_item_I, star_, creative_item,
                 empty_shell, SlimefunItems.STONE_CHUNK, empty_shell,
                 creative_item, uncontrollable_empty, creative_item_I,
         };
         ItemStack[] iceGenerator = {
-                creative_item_I, none_, creative_item,
+                creative_item_I, star_, creative_item,
                 uncontrollable_empty, new ItemStack(Material.WATER_BUCKET), uncontrollable_empty,
                 creative_item, empty_shell, creative_item_I,
         };
         ItemStack[] CGenerator = {
-                creative_item_I, none_, creative_item,
+                creative_item_I, star_, creative_item,
                 uncontrollable_empty, SlimefunItems.CARBON, uncontrollable_empty,
                 creative_item, none_, creative_item_I,
         };
         ItemStack[] commandBlockGenerator = {
-                creative_item_II, none_, creative_item_II,
+                creative_item_II, star_, creative_item_II,
                 empty_shell, new SlimefunItemStack("COMMAND_BLOCK_GETTER", Items.COMMAND_BLOCK_GETTER), empty_shell,
-                creative_item_II, none_, creative_item_II,
+                creative_item_II, star_, creative_item_II,
         };
         ItemStack[] generator = {
-                creative_item_I, qy, creative_item,
-                empty_shell, empty_shell, empty_shell,
+                creative_item_I, box_of_quantum, creative_item,
+                quantum1_, empty_shell, quantum1_,
                 qy, SlimefunItems.MAGIC_SUGAR, qy
         };
         ItemStack[] spiderEyeGenerator = {
-                creative_item_I, none_, creative_item,
+                creative_item_I, star_, creative_item,
                 uncontrollable_empty, new ItemStack(Material.SPIDER_EYE), uncontrollable_empty,
                 creative_item, none_, creative_item_I,
         };
         ItemStack[] beeGenerator = {
-                creative_item_I, none_, creative_item,
+                creative_item_I, star_, creative_item,
                 uncontrollable_empty, SlimefunItems.MAGIC_SUGAR, uncontrollable_empty,
                 creative_item, none_, creative_item_I,
         };
         ItemStack[] letterConstructor = {
-                creative_item_II, creative_item_II, creative_item_II,
+                creative_item_II, box_of_quantum, creative_item_II,
                 new ItemStack(Material.REPEATING_COMMAND_BLOCK), quantity_item, new ItemStack(Material.CHAIN_COMMAND_BLOCK),
-                uncontrollable_empty, uncontrollable_empty, uncontrollable_empty
+                uncontrollable_empty, box_of_quantum, uncontrollable_empty
         };
         ItemStack[] IDPutter = {
                 quantity_item, quantity_item, quantity_item,
-                qy, new ItemStack(Material.COMMAND_BLOCK), ch,
+                box_of_quantum, new ItemStack(Material.COMMAND_BLOCK), box_of_quantum,
                 creative_item, creative_item_I, creative_item_II
         };
         ItemStack[] IDChanger = {
                 quantity_item, final_item, quantity_item,
-                creative__, final_item, creative__,
-                qy, final_item, qy
+                final_item, final_item, final_item,
+                box_of_quantum, final_item, box_of_quantum
         };
+        ItemStack[] store = {
+                new ItemStack(Material.COBBLESTONE), new ItemStack(Material.COBBLESTONE), new ItemStack(Material.COBBLESTONE),
+                new ItemStack(Material.COBBLESTONE), empty_shell, new ItemStack(Material.COBBLESTONE),
+                new ItemStack(Material.COBBLESTONE), new ItemStack(Material.COBBLESTONE), new ItemStack(Material.COBBLESTONE)
+        };
+        ItemStack[] table = {
+                quantum1_, box_of_quantum, quantum1_,
+                empty_shell, cobblestone_[7], empty_shell,
+                quantum_, quantum_, quantum_
+        };
+        ItemStack[] finalC = {
+                box_of_quantum, box_of_quantum, box_of_quantum,
+                cobblestone_[9], cobblestone_[19], cobblestone_[9],
+                none_, new ItemStack(Material.COMMAND_BLOCK), none_
+        };
+        new FINAL_COBBLESTONE_GENERATOR(Items.MOMOTECH_FINAL, "FINAL_C_", Items.FINAL_C_, RecipeType.ENHANCED_CRAFTING_TABLE, finalC).register(MomoTech.getInstance());
+        new CobblestoneGeneratorFinal(Items.MOMOTECH_MACHINE, "MOMOTECH_COBBLESTONE_GENERATOR_FINAL", Items.MOMOTECH_COBBLESTONE_GENERATOR_FINAL, RecipeType.ENHANCED_CRAFTING_TABLE, table).register(MomoTech.getInstance());
+        new CobbleStoneStore(Items.MOMOTECH_MACHINE, "MOMOTECH_COBBLESTONE_STORE", Items.MOMOTECH_COBBLE_STONE_STORE, RecipeType.ENHANCED_CRAFTING_TABLE, store).register(MomoTech.getInstance());
         new IDPutter(Items.MOMOTECH_MACHINE, "MOMOTECH_ID_PUTTER", Items.MOMOTECH_ID_PUTTER, RecipeType.ENHANCED_CRAFTING_TABLE, IDPutter).register(MomoTech.getInstance());
         new IDChanger(Items.MOMOTECH_FINAL, "MOMOTECH_ID_CHANGER", Items.MOMOTECH_ID_CHANGER, RecipeType.ENHANCED_CRAFTING_TABLE, IDChanger).register(MomoTech.getInstance());
-        new LetterConstructor(Items.MOMOTECH_MACHINE, "MOMOTECH_LETTER_CONSTRUCOTR", Items.MOMOTECH_LETTER_CONSTRUCTOR, RecipeType.ENHANCED_CRAFTING_TABLE, letterConstructor).register(MomoTech.getInstance());
+        new LetterConstructor(Items.MOMOTECH_MACHINE, "MOMOTECH_LETTER_CONSTRUCTOR", Items.MOMOTECH_LETTER_CONSTRUCTOR, RecipeType.ENHANCED_CRAFTING_TABLE, letterConstructor).register(MomoTech.getInstance());
         new COMMAND_GENERATOR(Items.MOMOTECH_ORDINARY_MACHINE, "COMMAND_BLOCK_GENERATOR", Items.COMMAND_BLOCK_GENERATOR, RecipeType.ENHANCED_CRAFTING_TABLE, commandBlockGenerator).register(MomoTech.getInstance());
         new C_GENERATOR(Items.MOMOTECH_ORDINARY_MACHINE, "C_GENERATOR", Items.C_GENERATOR, RecipeType.ENHANCED_CRAFTING_TABLE, CGenerator).register(MomoTech.getInstance());
         new BEE_GENERATOR(Items.MOMOTECH_ORDINARY_MACHINE, "BEE_GENERATOR", Items.BEE_GENERATOR, RecipeType.ENHANCED_CRAFTING_TABLE, beeGenerator).register(MomoTech.getInstance());
