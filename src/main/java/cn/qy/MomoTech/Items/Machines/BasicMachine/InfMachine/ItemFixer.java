62da7379d24d97eeff7a2ae28ecec520abd6621f
QYhB05 <140108356+QYhB05@users.noreply.github.com>
2024-04-06 10:53:16 +0800

    Add files via upload

---
+++ src/main/java/cn/qy/MomoTech/Items/Machines/BasicMachine/InfMachine/ItemFixer.java
@@ -0,0 +1,70 @@
+package cn.qy.MomoTech.Items.Machines.BasicMachine.InfMachine;
+
+import cn.qy.MomoTech.GUI.AbstractGUI;
+import cn.qy.MomoTech.utils.Utils;
+import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
+import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+public class ItemFixer extends AbstractGUI {
+    public ItemFixer(ItemGroup itemGroup, String id, ItemStack it, RecipeType recipeType, ItemStack[] recipe) {
+        super(itemGroup, id, it, recipeType, recipe);
+    }
+
+    @Override
+    public int[] IN() {
+        return new int[0];
+    }
+
+    @Override
+    public int[] OUT() {
+        return new int[0];
+    }
+
+    @Override
+    public int[] EMPTY() {
+        return new int[]{4};
+    }
+
+    @NotNull
+    @Override
+    public String getMachineIdentifier() {
+        return "MOMOTECH_ITEM_FIXER";
+    }
+
+    @Override
+    public int[] getInputSlots() {
+        return new int[]{0, 1, 2, 3};
+    }
+
+    @Override
+    public int[] getOutputSlots() {
+        return new int[]{5, 6, 7, 8};
+    }
+
+    @Override
+    protected void findNextRecipe(BlockMenu inv) {
+        for (int i : getInputSlots()) {
+            if (Utils.checkOutput(inv, getOutputSlots())) return;
+            if (inv.getItemInSlot(i) == null) continue;
+            try {
+                SlimefunItem it1 = SlimefunItem.getByItem(inv.getItemInSlot(i));
+                if (it1 != null) {
+                    ItemStack it = new ItemStack(Objects.requireNonNull(SlimefunItem.getByItem(inv.getItemInSlot(i))).getItem());
+                    it.setAmount(inv.getItemInSlot(i).getAmount());
+                    inv.pushItem(it, getOutputSlots());
+                    inv.consumeItem(i, inv.getItemInSlot(i).getAmount());
+                } else {
+                    inv.pushItem(inv.getItemInSlot(i).clone(), getOutputSlots());
+                    inv.consumeItem(i, inv.getItemInSlot(i).getAmount());
+                }
+            } catch (Exception ignored) {
+            }
+        }
+    }
+}
---
+++ src/main/java/cn/qy/MomoTech/Items/Machines/BasicMachine/InfMachine/MonsterItemGenerator/MonsterItemGenerator4.java
@@ -0,0 +1,38 @@
+package cn.qy.MomoTech.Items.Machines.BasicMachine.InfMachine.MonsterItemGenerator;
+
+import cn.qy.MomoTech.GUI.MonsterItemGenerator;
+import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
+import io.github.thebusybiscuit.slimefun4.core.attributes.RecipeDisplayItem;
+import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MonsterItemGenerator4 extends MonsterItemGenerator implements RecipeDisplayItem {
+    public MonsterItemGenerator4(ItemGroup itemGroup, String id, ItemStack it, RecipeType recipeType, ItemStack[] recipe) {
+        super(itemGroup, id, it, recipeType, recipe);
+    }
+
+    @Override
+    public @NotNull String getMachineIdentifier() {
+        return "MOMOTECH_MONSTER_GENERATOR4";
+    }
+
+    @Override
+    public Material getItems() {
+        return Material.GUNPOWDER;
+    }
+
+    @NotNull
+    @Override
+    public List<ItemStack> getDisplayRecipes() {
+        List<ItemStack> item = new ArrayList<>(10);
+        item.add(new CustomItemStack(Material.GRAY_STAINED_GLASS_PANE, "&f可能的产物"));
+        item.add(new ItemStack(getItems(), 32));
+        return item;
+    }
+}
