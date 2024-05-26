c056b6e3278ffd9a54ee7b8aa0983a0a0a093994
QYhB05 <140108356+QYhB05@users.noreply.github.com>
2024-05-26 10:08:24 +0800

    Update MomoTech.java

--- src/main/java/cn/qy/MomoTech/MomoTech.java
+++ src/main/java/cn/qy/MomoTech/MomoTech.java
@@ -36,9 +36,8 @@ public class MomoTech extends JavaPlugin implements SlimefunAddon {
         getLogger().info("---------< MomoTech >-------");
         getLogger().info("|      Authors:QYhB05      |");
         getLogger().info("|      Plugin:MomoTech     |");
-        getLogger().info("|      Version:1.1.8       |");
+        getLogger().info("|      Version:1.1.9       |");
         getLogger().info("----------------------------");
-        getLogger().info("> INFO:MomoTech(乱码科技/陌陌科技) v1.1.8");
         getLogger().info("> 依赖项:SlimeFun4");
         getLogger().info("> 感谢Chenmose, skybule提供想法");
         getLogger().info("> QQ反馈群:827684043");
@@ -47,33 +46,29 @@ public class MomoTech extends JavaPlugin implements SlimefunAddon {
         getLogger().info("> 使用的SlimeFun4版本: RC-35");
         getLogger().info("> 使用的License:MIT");
         getLogger().info("> 下载链接:https://github.com/QYhB05/MomoTech/tree/main");
-        getLogger().info("> 感谢您的使用和支持, 欢迎提出更多想法以及建议");
-        getLogger().info("> 欢迎您为我们制作不同语言的版本, 也欢迎您魔改本插件");
         getLogger().info("----------------------------");
-        getLogger().info("=>载入附属中...");
-        getLogger().info("=>开始");
+        getLogger().info("载入附属中...");
         instance = this;
-        getLogger().info("=>开始注册监听器");
+        getLogger().info("开始注册监听器");
         getServer().getPluginManager().registerEvents(new Listeners(), this);
-        getLogger().info("=>监听器注册成功");
-        getLogger().info("=>初始化参数");
+        getLogger().info("监听器注册成功");
+        getLogger().info("初始化参数");
         init(getLogger(), getServer());
         logger = getLogger();
         server = getServer();
-        getLogger().info("=>开始注册物品");
+        getLogger().info("开始注册物品");
         ItemRegisterTask.run(getLogger());
-        getLogger().info("=>物品注册成功");
-        getLogger().info("=>开始注册机器");
+        getLogger().info("物品注册成功");
+        getLogger().info("开始注册机器");
         MachineRegisterTask.run(getLogger());
-        getLogger().info("=>机器注册成功");
-        getLogger().info("=>所有物品注册完毕, 附属已成功启动, 再次感谢您的支持");
+        getLogger().info("机器注册成功");
+        getLogger().info("所有物品注册完毕, 附属已成功启动, 再次感谢您的支持");
     }
 
     @Override
     public void onDisable() {
         // Logic for disabling the plugin...
         getLogger().info("MomoTech has been on disable.");
-        getLogger().info("=>MomoTech插件已关闭, 期待您的下次使用");
     }
 
     @Override
